f55e6b74f2ddc3ac801a632f5f97e275bd2c6896
Edison <fhp818@126.com>
2026-08-12 17:31:49 +0800

    feat: engine

---
+++ client/Assets/DBEngine/Runtime/Module/ResourceModule/ResourceLogger.cs
@@ -0,0 +1,25 @@
+﻿namespace DBEngine
+{
+    internal class ResourceLogger : YooAsset.ILogger
+    {
+        public void Log(string message)
+        {
+            DBEngine.Log.Info(message);
+        }
+
+        public void Warning(string message)
+        {
+            DBEngine.Log.Warning(message);
+        }
+
+        public void Error(string message)
+        {
+            DBEngine.Log.Error(message);
+        }
+
+        public void Exception(System.Exception exception)
+        {
+            DBEngine.Log.Fatal(exception.Message);
+        }
+    }
+}
